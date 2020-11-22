997e555408443264664b54a98bb95c6737a32477
chkra <9607474+chkra@users.noreply.github.com>
2020-11-22 13:58:42 +0100

    Initial Commit

---
+++ Tinka Bell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tinka Bell
+//
+//  Created by Christina Kratsch on 22.11.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
