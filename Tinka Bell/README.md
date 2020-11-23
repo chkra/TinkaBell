b30fd290105cbc36717dc61c8bae73342d5d64b2
chkra <9607474+chkra@users.noreply.github.com>
2020-11-23 22:10:01 +0100

    Initial Commit.

---
+++ Tinka Bell/README.md
@@ -0,0 +1,19 @@
+# Tinka Bell Readme
+
+## Versions
+
+### Version 1.0: 
+
+![](misc/v1.0.png)
+Initial version of the app showing three animal faces that play different sounds.
+
+
+
+
+## Attributions for external ressources
+
+
+* [Animal images](https://www.vecteezy.com/vector-art/426152-animal-faces) are taken from  <a href="https://www.vecteezy.com/free-vector/face">Vecteezy</a>
+* Music is taken from [Pixabay]("https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=51")")
+* Color Palette taken from [ColorHunt](https://colorhunt.co/palette/212737)
+
--- Tinka Bell/ViewController.swift
+++ Tinka Bell/ViewController.swift
@@ -6,14 +6,65 @@
 //
 
 import UIKit
+import AVFoundation
+
+
+let soundFilenames = ["audio1",
+                      "audio2",
+                      "audio3"]
+
+var audioPlayerArray = [AVAudioPlayer]()
+
+
 
 class ViewController: UIViewController {
 
+    var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for sound in soundFilenames {
+          do {
+               let url = URL(fileURLWithPath: Bundle.main.path(forResource: sound, ofType: "mp3")!)
+               let audioPlayer = try AVAudioPlayer(contentsOf: url)
+               audioPlayerArray.append(audioPlayer)
+          } catch {
+               print(error)
+               audioPlayerArray.append(AVAudioPlayer())
+          }
+        }
     }
 
-
+    @IBAction func animalButton1Pressed(_ sender: Any) {
+        print("Brumm brumm!")
+        audioPlayer.stop()
+        audioPlayer = audioPlayerArray[0]
+        audioPlayer.currentTime = 0
+        audioPlayer.play()
+    }
+    
+    @IBAction func animalButton2Pressed(_ sender: Any) {
+        print("Mümmel mümmel!")
+        audioPlayer.stop()
+        audioPlayer = audioPlayerArray[1]
+        audioPlayer.currentTime = 0
+        audioPlayer.play()
+    }
+    
+    @IBAction func animalButton3Pressed(_ sender: Any) {
+        print("Wuff wuff!")
+        audioPlayer.stop()
+        audioPlayer = audioPlayerArray[2]
+        audioPlayer.currentTime = 0
+        audioPlayer.play()
+    }
+    
+    @IBAction func stopAllButtonPressed(_ sender: Any) {
+        for player in audioPlayerArray {
+            player.stop()
+            player.currentTime = 0
+        }
+    }
 }
 
